06d616021cc05507015e1edddfcd5f1cfbe4eac1
wombito <haydeejbb@gmail.com>
2026-01-08 13:52:52 -0300

    feat: add validation decorators to CreateQuestionDto

--- src/questions/dto/create-question.dto.ts
+++ src/questions/dto/create-question.dto.ts
@@ -1,4 +1,14 @@
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateQuestionDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(30)
     riddle: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
     answer: string;
 }
